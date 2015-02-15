418ad4ca2b1ea7b1840d19cb6741166b50b83ab3
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2015-02-16 00:11:51 +0900

    Add brew test openshift

--- openshift.rb
+++ openshift.rb
@@ -1,8 +1,9 @@
+HOMEBREW_OPENSHIFT_VERSION="v0.3.1"
+
 class Openshift < Formula
   homepage "https://github.com/openshift/origin"
-  url "https://github.com/openshift/origin.git", :tag => "v0.3.1"
+  url "https://github.com/openshift/origin.git", :tag => "#{HOMEBREW_OPENSHIFT_VERSION}"
   head "https://github.com/openshift/origin.git", :branch => "master"
-
   depends_on "go" => :build
 
   def install
@@ -12,6 +13,11 @@ class Openshift < Formula
     system "make", "all", "WHAT=cmd/openshift", "GOFLAGS=-v"
 
     bin.install "#{output_dir}/openshift"
-    bin.install "#{output_dir}/osc"
+    bin.install_symlink "openshift" => "osc"
+  end
+
+  test do
+    assert_match /^OpenShift for Admins/, shell_output("#{bin}/openshift 2>&1", 0)
+    assert_match /^OpenShift Client/, shell_output("#{bin}/osc 2>&1", 0)
   end
 end
