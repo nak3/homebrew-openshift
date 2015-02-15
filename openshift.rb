e6a46999ff24b604d70bc0aec492fa87590d3499
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2015-02-15 22:32:04 +0900

    Fixed some mistakes

--- openshift.rb
+++ openshift.rb
@@ -1,4 +1,4 @@
-class OpenShift < Formula
+class Openshift < Formula
   homepage "https://github.com/openshift/origin"
   url "https://github.com/openshift/origin.git", :tag => "v0.3.1"
   head "https://github.com/openshift/origin.git", :branch => "master"
@@ -6,10 +6,10 @@ class OpenShift < Formula
   depends_on "go" => :build
 
   def install
-    arch = MacOS.prefer_64_bit ? "amd64" : "x86"
-    output_dir = "_output/local/bin/darwin/#{arch}"
+    arch = MacOS.prefer_64_bit? ? "amd64" : "x86"
+    output_dir = "_output/local/go/bin"
 
-    system "make" "all" "WHAT=cmd/openshift", "GOFLAGS=-v"
+    system "make", "all", "WHAT=cmd/openshift", "GOFLAGS=-v"
 
     bin.install "#{output_dir}/openshift"
     bin.install "#{output_dir}/osc"
