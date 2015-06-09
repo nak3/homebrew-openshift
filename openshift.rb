b6c11413f923142e170d2c284dc9a65bd0de1f1d
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2015-06-10 08:34:14 +0900

    Update to v0.6

--- openshift.rb
+++ openshift.rb
@@ -1,4 +1,4 @@
-HOMEBREW_OPENSHIFT_VERSION="v0.5.4"
+HOMEBREW_OPENSHIFT_VERSION="v0.6"
 
 class Openshift < Formula
   homepage "https://github.com/openshift/origin"
@@ -12,12 +12,12 @@ class Openshift < Formula
     system "make", "all", "WHAT=cmd/openshift", "GOFLAGS=-v"
 
     bin.install "#{output_dir}/openshift"
-    bin.install_symlink "openshift" => "osc"
-    bin.install_symlink "openshift" => "osadm"
+    bin.install_symlink "openshift" => "oc"
+    bin.install_symlink "openshift" => "oadm"
   end
 
   test do
     assert_match /^OpenShift for Admins/, shell_output("#{bin}/openshift 2>&1", 0)
-    assert_match /^OpenShift Client/, shell_output("#{bin}/osc 2>&1", 0)
+    assert_match /^OpenShift Client/, shell_output("#{bin}/oc 2>&1", 0)
   end
 end
